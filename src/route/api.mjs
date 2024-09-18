3d4d16b1c06a8bd39ce6d6b7f4499c0742796688
Lainey Liu <laineyl@andrew.cmu.edu>
2024-09-18 12:41:26 -0700

    Add checking use before register (#33)

--- src/route/api.mjs
+++ src/route/api.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, authenticateJWT } from "../auth.js";
+import { register, authenticateJWT, check } from "../auth.js";
 
 const router = express.Router();
 // router.use(express.json());
